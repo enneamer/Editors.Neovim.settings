878edf144fb3cd57403ab7470931e2a40ecbc145
Enneamer <enneamer@enneamer.is>
2024-02-01 17:40:24 +0800

    fix: tune the tree plugin a bit

--- plugins/core.lua
+++ plugins/core.lua
@@ -4,6 +4,14 @@ return {
     opts = {
       filesystem = {
         bind_to_cwd = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+        follow_current_file = {
+          leave_dirs_open = false,
+        },
       },
     },
   },
