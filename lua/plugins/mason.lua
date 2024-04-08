ee8c545fc8c176799c96287657cc98f9199b7e87
Enneamer <enneamer@enneamer.is>
2024-04-08 17:09:50 +0800

    fix: bring back ruff formatter
    The old formatter was removed in favour of ruff-lsp, which for unknown
    reason does not format well.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,11 +21,33 @@ return {
     opts = function(_, opts)
       local null_ls = require "null-ls"
       local methods = require "null-ls.methods"
+      local helpers = require "null-ls.helpers"
+
       opts.handlers["ansiblelint"] = function(_0, _1)
         local config = vim.deepcopy(null_ls.builtins.diagnostics.ansiblelint)
         config["method"] = { methods.internal.DIAGNOSTICS_ON_OPEN, methods.internal.DIAGNOSTICS_ON_SAVE }
         null_ls.register(config)
       end
+
+      local config = helpers.make_builtin {
+        name = "ruff",
+        meta = {
+          url = "https://github.com/charliermarsh/ruff/",
+          description = "An extremely fast Python linter, written in Rust.",
+        },
+        method = methods.internal.FORMATTING,
+        filetypes = { "python" },
+        generator_opts = {
+          command = "ruff",
+          args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
+          to_stdin = true,
+        },
+        factory = helpers.formatter_factory,
+      }
+      null_ls.register(config)
+
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff" })
+
       -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       --   "prettier",
       --   "stylua",
